673dbb56251d5712d72cc4a86ba966ca2112aa3b
goworldte <150192570+goworldte@users.noreply.github.com>
2025-06-18 00:33:14 +0300

    Extensions.cs

---
+++ StockSharp/Configuration/Extensions.cs
@@ -0,0 +1,15 @@
+﻿namespace StockSharp.Configuration;
+
+/// <summary>
+/// Extension class.
+/// </summary>
+public static class Extensions
+{
+	/// <summary>
+	/// </summary>
+	public static long? TryGetProductId()
+		=> Paths
+			.EntryAssembly?
+			.GetAttribute<ProductIdAttribute>()?
+			.ProductId;
+}
